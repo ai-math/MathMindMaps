3718e3c633e892a8e9b708ea5412d0833f1c3f2c
AKFish <akfish@gmail.com>
2013-08-19 09:46:05 +0800

    Initial commit for GitHub Pages

---
+++ js/Math.js
@@ -0,0 +1,62 @@
+/**
+ * Config MathJax to support LaTex style inline math
+ */
+function initializeLaTexInlineMath() {
+    MathJax.Hub.Config({
+	tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}
+    });
+    console.log("Math > Initialize");
+}
+
+/**
+ * Notify MathJax to update all math equations in the document
+ */
+function updateMathEquations() {
+    MathJax.Hub.Queue(['Typeset',MathJax.Hub]);
+    console.log("Math > update: all");
+}
+
+/**
+ * Notify MathJax to update math equations within elements of given id
+ */
+function updateMathEquationById(id) {
+    MathJax.Hub.Queue(['Typeset',MathJax.Hub, id]);
+    console.log("Math > update: " + id);
+}
+
+var catx = catx || {};
+
+catx.MathController = function(eventBus) {
+
+    var self = this;
+    /**
+     * Initialise.
+     * 
+     * @private
+     */
+    this.init = function() {
+
+	eventBus.subscribe(mindmaps.Event.MAP_LOADED, this.mapLoaded
+			   .bind(this));
+	eventBus.subscribe(mindmaps.Event.NODE_BEGIN_EDITING, this.nodeBeginEditing
+			   .bind(this));
+	eventBus.subscribe(mindmaps.Event.NODE_END_EDITING, this.nodeEndEditing
+			   .bind(this));
+    };
+
+    /**
+     * Handler for map loaded event.
+     */
+    this.mapLoaded = function(map) {
+	updateMathEquations();
+    };
+    
+    this.nodeBeginEditing = function(node) {
+	var captionElem = $("#" + node.getCaptionId()).text(node.text.caption);
+    };
+
+    this.nodeEndEditing = function(node) {
+	updateMathEquationById(node.getCaptionId());
+    };
+    this.init();
+}
